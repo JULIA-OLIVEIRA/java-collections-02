9c2088b69d042c8d8eb96021b46c4a8b753ed812
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 20:08:42 -0300

    segunda alteracao

--- java-collections-02/src/br/fai/javacollections02/Main.java
+++ java-collections-02/src/br/fai/javacollections02/Main.java
@@ -1,5 +1,7 @@
 package br.fai.javacollections02;
 
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -9,9 +11,45 @@ public class Main {
 
 	}
 	
+	private Map<String, String> mapa = new HashMap<String, String>();
+	
 	private void start() {
 		
+		adicionarItensNoMapa();
+		exibirValorDoMapaPelaChave("PA");
+		exibirValorDoMapaPelaChave("valor que não existe");
+		iterarMapa();
 		
+	}
+	
+	private void adicionarItensNoMapa() {
+		
+		mapa.put("SRS", "Santa Rita do Sapucai");
+		mapa.put("PA", "Pouso Alegre");
+		mapa.put("SRS", "Santa Rita");
+		mapa.put("SRS", "Santa");
+		mapa.put("ITA", "Itajuba");
+		mapa.put("ZORO", "Conceição dos Ouros");
+		mapa.put("CAXU", "Cachoeira de Minas");
+	}
+	
+	private void exibirValorDoMapaPelaChave(String chave) {
+		
+		String valorDoItemNoMapa = mapa.get(chave);
+		if (valorDoItemNoMapa == null
+				|| valorDoItemNoMapa.isEmpty())
+			System.out.println("esta chave não" + "está presente no mapa: " + chave);
+		System.out.println("o valor dessa chave é: " + mapa.get(chave));
+	}else {
+		System.out.println("o valor dessa chave é: " + mapa.get(chave));
 	}
 
 }
+
+	private void iterarMapa() {
+		
+		System.out.println(" ------- ");
+		for(String chave: mapa.keySet()) {
+			System.out.println("chave: " + chave + "| valor:" + mapa.get(chave));
+		}
+	}
